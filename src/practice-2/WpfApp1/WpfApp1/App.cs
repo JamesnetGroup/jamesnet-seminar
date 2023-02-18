1a9a7b526140a04c819c289071c7559c0f289e91
JAEWUNG LEE <james@jamesnet.dev>
2023-02-18 13:29:43 +0900

    Updates

---
+++ src/practice-2/WpfApp1/WpfApp1/App.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    internal class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Window1 win = new();
+            win.ShowDialog();
+        }
+    }
+}
---
+++ src/practice-2/WpfApp1/WpfApp1/Starter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Starter
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            App app = new();
+            app.Run();
+        }
+    }
+}

ec12a65dcb8d4ace86fbdfcb6e5da8c005ec6dab
JAEWUNG LEE <james@jamesnet.dev>
2023-03-04 14:27:36 +0900

    Updates

---
+++ src/practice-4/GeometryApp/GeometryApp/GeoIcon.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeometryApp
+{
+    public class GeoIcon : ContentControl
+    {
+        private static string APPLE = "M18.71,19.5C17.88,20.74 17,21.95 15.66,21.97C14.32,22 13.89,21.18 12.37,21.18C10.84,21.18 10.37,21.95 9.1,22C7.79,22.05 6.8,20.68 5.96,19.47C4.25,17 2.94,12.45 4.7,9.39C5.57,7.87 7.13,6.91 8.82,6.88C10.1,6.86 11.32,7.75 12.11,7.75C12.89,7.75 14.37,6.68 15.92,6.84C16.57,6.87 18.39,7.1 19.56,8.82C19.47,8.88 17.39,10.1 17.41,12.63C17.44,15.65 20.06,16.66 20.09,16.67C20.06,16.74 19.67,18.11 18.71,19.5M13,3.5C13.73,2.67 14.94,2.04 15.94,2C16.07,3.17 15.6,4.35 14.9,5.19C14.21,6.04 13.07,6.7 11.95,6.61C11.8,5.46 12.36,4.26 13,3.5Z";
+        private static string FACEBOOK = "M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z";
+
+        public static readonly DependencyProperty IconTypeProperty =
+            DependencyProperty.Register("IconType", typeof(IconType), typeof(GeoIcon), new PropertyMetadata(IconType.Apple, IconTypeChanged));
+
+        private static void IconTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            GeoIcon control = d as GeoIcon;
+
+            if (control != null)
+            {
+                switch(control.IconType)
+                {
+                    case IconType.Apple: control.Data = Geometry.Parse(APPLE); break;
+                    case IconType.Facebook: control.Data = Geometry.Parse(FACEBOOK); break;
+                }
+            }
+        }
+
+        public IconType IconType
+        {
+            get { return (IconType)GetValue(IconTypeProperty); }
+            set { SetValue(IconTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(Geometry), typeof(GeoIcon), new PropertyMetadata(null));
+
+        public Geometry Data
+        {
+            get { return (Geometry)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        static GeoIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GeoIcon), new FrameworkPropertyMetadata(typeof(GeoIcon)));
+        }
+    }
+}
